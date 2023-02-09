6798000a2bb81bd84deca38139b9b30b9a08fce0
Peter <trdin.peter@gmail.com>
2023-02-09 19:12:05 +0100

    Writting out taks +UI

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent {
+  @Input() task: Task = {
+    text: '',
+    day: '',
+    reminder: false
+  };
+
+  faTimes = faTimes;
+
+}
